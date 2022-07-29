8842eaf1fa322347d0f6cdd983f0819adc6312d9
Vivek <vivekmunde@gmail.com>
2022-07-29 06:02:08 +0530

    removed TStyles export

--- stylo/theme-context.ts
+++ stylo/theme-context.ts
@@ -1,22 +1,20 @@
 import React from 'react';
 import { ImageStyle, TextStyle, ViewStyle } from 'react-native';
 
-export type TStyles = {
-  IconStyles: Record<string, TextStyle>;
-  ImageBackgroundStyles: Record<string, ViewStyle>;
-  ImageStyles: Record<string, ImageStyle>,
-  SafeAreaViewStyles: Record<string, ViewStyle>,
-  ScrollViewStyles: Record<string, ViewStyle>,
-  ScrollViewContentContainerStyles: Record<string, ViewStyle>,
-  TextInputStyles: Record<string, TextStyle>,
-  TextStyles: Record<string, TextStyle>,
-  TouchableStyles: Record<string, ViewStyle>,
-  ViewStyles: Record<string, ViewStyle>,
-};
-
 export type TContext = {
-  variables: Record<string, string|number>;
-  styles: TStyles;
+  variables: Record<string, string | number>;
+  styles: {
+    IconStyles: Record<string, TextStyle>;
+    ImageBackgroundStyles: Record<string, ViewStyle>;
+    ImageStyles: Record<string, ImageStyle>,
+    SafeAreaViewStyles: Record<string, ViewStyle>,
+    ScrollViewStyles: Record<string, ViewStyle>,
+    ScrollViewContentContainerStyles: Record<string, ViewStyle>,
+    TextInputStyles: Record<string, TextStyle>,
+    TextStyles: Record<string, TextStyle>,
+    TouchableStyles: Record<string, ViewStyle>,
+    ViewStyles: Record<string, ViewStyle>,
+  };
 };
 
 export default React.createContext<TContext>({
